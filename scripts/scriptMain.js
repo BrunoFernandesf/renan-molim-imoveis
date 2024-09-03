881fd87f327f48b2d745ad92ccec05b94ac5a439
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-09-03 15:21:11 -0300

    ajuste de lógica para a pagina main

--- scripts/scriptMain.js
+++ scripts/scriptMain.js
@@ -1,5 +1,6 @@
 const imoveisRel = document.querySelectorAll(".imovel-lancamento");
 const imoveisTren = document.querySelectorAll(".imovel-trending");
+const imoveisSale = document.querySelectorAll(".imovel-sale");
 
 fetch("resources/json/imoveis.json")
 
@@ -10,9 +11,10 @@ fetch("resources/json/imoveis.json")
 
     //Lançamentos
     const imovelrelease = data.filter(imovel => imovel.release == true)
+    const limitRelease = 3;
 
     imovelrelease.forEach((imovel, index) => {
-        if (imoveisRel[index]){
+        if (imoveisRel[index] && index < limitRelease){
             imoveisRel[index].innerHTML = `
             <img src="${imovel.img}" alt="${imovel.name}">
             <h3>${imovel.name}</h3>
@@ -25,10 +27,27 @@ fetch("resources/json/imoveis.json")
 
     //Mais procurados
     const imovelTrending = data.filter(imovel => imovel.trending == true)
+    const limitTrend = 3;
 
     imovelTrending.forEach((imovel, index) =>{
-        if(imoveisTren[index]){
+        if(imoveisTren[index] && index < limitTrend){
             imoveisTren[index].innerHTML = `
+            <img src="${imovel.img}" alt="${imovel.name}">
+            <h3>${imovel.name}</h3>
+            <p>${imovel.location}</p>
+            <p>${imovel.value}</p>
+            <p>${imovel.details}</p>
+        `;
+        }
+    });
+
+    //Imóveis a venda
+    const imovelSale = data.filter(imovel => imovel.type == "Sale")
+    const limitSale = 3;
+
+    imovelSale.forEach((imovel, index) =>{
+        if(imoveisSale[index] && index < limitSale){
+            imoveisSale[index].innerHTML = `
             <img src="${imovel.img}" alt="${imovel.name}">
             <h3>${imovel.name}</h3>
             <p>${imovel.location}</p>
